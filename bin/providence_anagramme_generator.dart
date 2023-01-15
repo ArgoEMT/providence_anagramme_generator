65a46cc78c388a5398f429197c0228121badfd08
Victor Daumas <victor.daumas@outlook.fr>
2023-01-15 14:32:00 +0100

    chore: init dart

---
+++ bin/providence_anagramme_generator.dart
@@ -0,0 +1,5 @@
+import 'package:providence_anagramme_generator/providence_anagramme_generator.dart' as providence_anagramme_generator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${providence_anagramme_generator.calculate()}!');
+}
---
+++ test/providence_anagramme_generator_test.dart
@@ -0,0 +1,8 @@
+import 'package:providence_anagramme_generator/providence_anagramme_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
